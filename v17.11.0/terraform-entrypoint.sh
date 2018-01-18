d3a14308a4e1d3f2e0c6a281d31d7503c9588112
thomasriley <thomasriley@users.noreply.github.com>
2018-01-18 17:07:00 +0000

    More permissions fixes

--- v17.11.0/terraform-entrypoint.sh
+++ v17.11.0/terraform-entrypoint.sh
@@ -4,11 +4,11 @@
 mkdir /home/go/.ssh
 echo $GIT_SSH_KEY | base64 -d > /home/go/.ssh/id_rsa
 chmod 0600 /home/go/.ssh/id_rsa
-chown go: /home/go/.ssh/id_rsa
 
 # Set the known host from environment variable
 echo $GIT_KNOWN_HOST | base64 -d > /home/go/.ssh/known_hosts
-chown go: /home/go/.ssh/known_hosts
+
+chown -R go: /home/go/.ssh
 
 # Launch the GoCD entrypoint
 exec /docker-entrypoint.sh
