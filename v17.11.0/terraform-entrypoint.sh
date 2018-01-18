b768cf22bc0c9d0dbff5a415b28a927c3fb42613
thomasriley <thomasriley@users.noreply.github.com>
2018-01-18 15:30:05 +0000

    Set a ssh known host entry for the git server

--- v17.11.0/terraform-entrypoint.sh
+++ v17.11.0/terraform-entrypoint.sh
@@ -5,5 +5,8 @@ mkdir /home/go/.ssh
 echo $GIT_SSH_KEY | base64 -d > /home/go/.ssh/id_rsa
 chmod 0600 /home/go/.ssh/id_rsa
 
+# Set the known host from environment variable
+echo $GIT_KNOWN_HOST | base64 -d > /home/go/.ssh/known_hosts
+
 # Launch the GoCD entrypoint
 exec /docker-entrypoint.sh
